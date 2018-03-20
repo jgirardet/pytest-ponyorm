167d6b635568070caea6582c3352e4d680df00f4
Jimmy Girardet <test@ma.fr>
2018-03-20 08:59:56 +0100

    deploy

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-ponyorm',
-    version='0.2.3',
+    version='0.2.4',
     author='Jimmy Girardet',
     author_email='ijkl@netc.fr',
     maintainer='Jimmy Girardet',
